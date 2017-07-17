fa1f6cdf9f7871e1d052e8a298fd1fe12de60a71
Alex Safatli <safatli@cs.dal.ca>
2017-07-17 18:38:02 -0300

    ONEE-SAMA, PLEASE

--- src/main/java/net/dirtydeeds/discordsoundboard/chat/PlayRandomProcessor.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/chat/PlayRandomProcessor.java
@@ -38,7 +38,7 @@ public class PlayRandomProcessor extends SingleArgumentChatCommandProcessor {
     		LOG.info("Played \"" + filePlayed + "\" in server " + event.getGuild().getName());
     		StyledEmbedMessage em = StyledEmbedMessage.forSoundFile(bot, file, 
           "You've Played a Random Sound", desc + event.getAuthor().getAsMention());
-        em.addFooterText("Requested by " + event.getAuthor().getName());
+        em.addFooterText("For " + event.getAuthor().getName());
         em.setFooterIcon(event.getAuthor().getEffectiveAvatarUrl());
     		embed(event, em);
   		}
