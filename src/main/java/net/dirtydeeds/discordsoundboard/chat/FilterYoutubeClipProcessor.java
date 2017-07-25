0618e92569173a4c44fc0ef45acb165c7c63ecf0
Alex Safatli <safatli@cs.dal.ca>
2017-07-25 13:18:11 -0300

    Exactly as planned.

--- src/main/java/net/dirtydeeds/discordsoundboard/chat/FilterYoutubeClipProcessor.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/chat/FilterYoutubeClipProcessor.java
@@ -5,12 +5,12 @@ import java.util.regex.*;
 import net.dirtydeeds.discordsoundboard.service.SoundboardBot;
 import net.dv8tion.jda.core.entities.*;
 
-public class FilterTwitchClipProcessor extends FilterChatProcessor {
+public class FilterYoutubeClipProcessor extends FilterChatProcessor {
 
   private static final String CHANNEL = "clips";
 
   public FilterYoutubeClipProcessor(SoundboardBot bot) {
-    super(Pattern.compile(".*youtube\\.com/watch/.*"), CHANNEL, bot);
+    super(Pattern.compile(".*youtube\\.com/watch.*"), CHANNEL, bot);
   }
  
 }
