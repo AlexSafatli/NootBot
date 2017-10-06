5e682bd5498f073e1dff22cdf0c1976a89276109
Alex Safatli <asafatli@beyondtrust.com>
2017-10-06 08:22:32 -0300

    BOOOOOOOOOOOOOOOOSSSSSSSTTTTTTTTTTOOOOOOOOOOO

--- src/main/java/net/dirtydeeds/discordsoundboard/chat/PlaySoundProcessor.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/chat/PlaySoundProcessor.java
@@ -68,7 +68,7 @@ public class PlaySoundProcessor extends SingleArgumentChatCommandProcessor {
 			}
 			LOG.info("Suggestion: " + suggestion);
 			sendBadSoundMessage(event, name, suggestion, user);
-		} else if (bot.isMuted()) {
+		} else if (event.getGuild() != null && bot.isMuted(event.getGuild())) {
 			w(event, "I'm muted! \u2014 " + user.getName());
 		} else play(event, name);
 	}
