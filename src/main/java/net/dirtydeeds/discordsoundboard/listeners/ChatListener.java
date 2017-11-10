b8c18e363051e0716a9a8ecfe491bdd802940ffa
Alex Safatli <asafatli@beyondtrust.com>
2017-11-10 09:46:12 -0400

    Add favorite phrase processors

--- src/main/java/net/dirtydeeds/discordsoundboard/listeners/ChatListener.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/listeners/ChatListener.java
@@ -94,6 +94,8 @@ public class ChatListener extends AbstractListener {
     processors.add(new ReportBugProcessor(".bug",                     bot));
     processors.add(new InviteBotProcessor(".invite",                  bot));
     processors.add(new ServerDonationMessageProcessor(".donate",      bot));
+    processors.add(new FavoritePhraseProcessor(".likephrase",         bot));
+    processors.add(new UnfavoritePhraseProcessor(".dislikephrase",    bot));
     processors.add(new TempVoiceChannelProcessor(".tmpvoice",         bot));
     processors.add(new SoundAttachmentProcessor(                      bot));
     processors.add(new FilterTwitchClipProcessor(                     bot));
